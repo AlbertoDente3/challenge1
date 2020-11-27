f236d3aaf42474f2d1f0cddf04dc6ef91e67d8ab
Alberto Dente <alberto@pop-os.localdomain>
2020-11-28 00:56:25 +0100

    Aggiunto .trim alle stringhe

--- scriptCSV.js
+++ scriptCSV.js
@@ -1,76 +1,69 @@
-console.log()
+var fs = require('fs')
+
+
+
+
 
 var parametriPassati = process.argv.slice(2)
-
-
-
-console.log('Argomenti')
-parametriPassati.forEach((parametro, index) => {
-    console.log(index, ' Valore:', parametro)
-})
-console.log()
-
-var fs = require('fs')
 
 var percorsoFile = parametriPassati[0]
 var indiceColonnaDaCercare = parametriPassati[1]
 var valoreDaCercare = parametriPassati[2]
 var arrayJsonPersone = []
 
+
 /* leggo */
-var documento = fs.readFileSync('./data/avengers', 'utf8')
+var documento = fs.readFileSync('./data/names2', 'utf8')
 
 /* Estraggo le righe */
-let singoliRecord = documento.split(';')
-singoliRecord.pop()
+let arrayRecord = documento.split(';')
+
+/* Rimuovo l'ultimo record se vuoto */
+if (arrayRecord[arrayRecord.length - 1].trim() === '') {
+    arrayRecord.pop()
+}
 
 /* Creo il Json */
-singoliRecord.forEach((dato) => {
+arrayRecord.forEach((dato) => {
     let tmpArray = dato.split(',')
 
     arrayJsonPersone.push({
-        id: tmpArray[0],
-        cognome: tmpArray[1],
-        nome: tmpArray[2],
-        dataNascita: tmpArray[3],
+        id: tmpArray[0].trim(),
+        cognome: tmpArray[1].trim(),
+        nome: tmpArray[2].trim(),
+        dataNascita: tmpArray[3].trim(),
     })
 })
 
-
-console.log("lavoro con: ");
-
-console.log(cerca(indiceColonnaDaCercare,valoreDaCercare));
-
-
-
-
+/* Richiamo la funzione di ricerca */
+console.log(cerca(indiceColonnaDaCercare, valoreDaCercare))
 
 /* Funzioni */
 function cerca(indice, valore) {
+    /* Creo un array in cui metto le cose che corrispondono all uguaglianza */
     let daRitornare = []
 
+    /* Entro nella prima riga */
     for (let i = 0; i < arrayJsonPersone.length; i++) {
         const el = arrayJsonPersone[i]
 
         let nColonne = Object.keys(el).length
+        /* Cerco nele colonne */
         for (let j = 0; j < nColonne; j++) {
-            
+            /* Se la colonna e uguale alla colonna che sto cercando prosegue nel controlllo */
+            /* TODO volendo si potrebbe ottimizzare con una ricerca dicotomica */
             if (j === parseInt(indice)) {
-                /* console.log('Valore: ', j, el[Object.keys(el)[j]]) */
+                
                 datoConfronto = el[Object.keys(el)[j]]
 
                 if (datoConfronto === valore) daRitornare.push(arrayJsonPersone[i])
             }
         }
 
-  
-      
         console.log()
     }
 
-   
     return daRitornare
 }
-
 
 /* folder color #FFD580 */
